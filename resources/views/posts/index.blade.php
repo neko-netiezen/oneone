2a42e77c22dcd5a53dc568336957f349edccbbe3
neko-netiezen <201l2018@st.kumamoto-u.ac.jp>
2023-02-04 17:43:39 +0900

    clothmapのweb表示

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>ClothMap</title>
+    </head>
+    <body>
+        <div class="tabs">
+            <input id="home" type="radio" name="tab_item" checked>
+            <label class="tab_item" for="home">Home</label>
+            <input id="tops" type="radio" name="tab_item">
+            <label class="tab_item" for="tops">トップス</label>
+            <input id="bottoms" type="radio" name="tab_item">
+            <label class="tab_item" for="bottoms">ボトムス</label>
+            <input id="shoes" type="radio" name="tab_item">
+            <label class="tab_item" for="shoes">シューズ</label>
+            <input id="items" type="radio" name="tab_item">
+            <label class="tab_item" for="items">アイテム</label>
+            <input id="recommend" type="radio" name="tab_item">
+            <label class="tab_item" for="recomend">おすすめ</label>
+            <input id="post" type="radio" name="tab_item">
+            <label class="tab_item" for="post">投稿</label>
+            <div class="tab_content" id="home_content">
+                <div class="tab_content_description">
+                    <div class="c-txtsp">
+                        <div class="title_box">
+                            <img src="https://p4.wallpaperbetter.com/wallpaper/70/897/270/ultrawide-snow-road-wallpaper-preview.jpg" alt="タイトル壁紙" style="width: 100%; object-fit: cover; object-position: 50% 50%;">
+                            <div>
+                                <p class="title1">ClothMap</p>
+                            </div>
+                            <div>
+                                <p class="login">ログイン</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="topic_img">
+                    <img src="./images/shirts.png">
+                    <img src="./images/pants.png">
+                    <img src="./images/bag.png">
+                    <img src="./images/shoes.png">
+                </div>
+            </div>
+            <div class="tab_content" id="tops_content">
+                <div class="tab_content_description">
+                    <p class="c-txtsp"></p>
+                </div>
+            </div>
+            <div class="tab_content" id="bottoms_content">
+                <div class="tab_content_description">
+                    <p class="c-txtsp"></p>
+                </div>
+            </div>
+            <div class="tab_content" id="shoes_content">
+                <div class="tab_content_description">
+                    <p class="c-txtsp"></p>
+                </div>
+            </div>
+            <div class="tab_content" id="items_content">
+                <div class="tab_content_description">
+                    <p class="c-txtsp"></p>
+                </div>
+            </div>
+            <div class="tab_content" id="login_content">
+                <div class="tab_content_description">
+                    <p class="c-txtsp"></p>
+                    <a href="file:///C:/Users/alise/OneDrive/%E3%83%87%E3%82%B9%E3%82%AF%E3%83%88%E3%83%83%E3%83%97/%E3%83%AC%E3%83%90%E3%83%86%E3%83%83%E3%82%AF%E3%82%AB%E3%83%AC%E3%83%83%E3%82%B8/Practice01_3.html"><p class="title2">新規ログイン</p></a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
